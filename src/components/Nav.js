19bc2b66ea9d40d49f5abf5ff043bf9209c91025
Luke G <lukejgranered@gmail.com>
2022-05-07 14:37:35 -0500

    init commit

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const pages = [
+    { name: 'About', ref: '#about' },
+    { name: 'Projects', ref: '#projects' },
+    { name: 'Resume', ref: '#resume' },
+    { name: 'Contact', ref: '#contact'}
+];
+
+export default Nav;
